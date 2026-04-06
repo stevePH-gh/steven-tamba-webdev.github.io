388cf17d944926702a0b0e5a8321d5fec607dfd5
Steven Tamba <122578774+stevePH-gh@users.noreply.github.com>
2026-04-06 15:32:46 +0800

    Added dark mode toggle

--- script/script.js
+++ script/script.js
@@ -24,3 +24,27 @@ function highlightContact() {
     contact.classList.remove('contact-glow');
   }, 800);
 }
+
+function toggleDarkMode() {
+  var body = document.body
+  var intPanel = document.getElementById("introduction-panel")
+  var sections = document.getElementsByClassName("title-sections")
+  var header = document.getElementById("home")
+
+  var btn = document.getElementById("toggleButton")
+
+  body.classList.toggle("body-light-mode");
+  intPanel.classList.toggle("introduction-panel-light");
+  header.classList.toggle("navHdr-light")
+  btn.classList.toggle("btnToggle-light")
+
+  for (i = 0; i < sections.length; i++) {
+    sections[i].classList.toggle("title-sections-light");
+  }
+
+  if (btn.innerHTML === "Light") {
+    btn.innerHTML = "Dark"
+  } else {
+    btn.innerHTML = "Light"
+  }
+}
